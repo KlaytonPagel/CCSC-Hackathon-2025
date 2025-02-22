418cfe3bb9d91f94b859db3c41f3c77f210fd994
root <root@Personal-Dev.home>
2025-02-22 04:10:32 +0000

    Added directional lightw

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.body.appendChild( renderer.domElement );
 
 // -------------------------------------------------------- Setting up the Camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 5;
+camera.position.z = 10;
 
 // -------------------------------------------------------- Setting up the scene
 const scene = new THREE.Scene();
@@ -43,26 +43,33 @@ mesh.add(wireMesh);
 const torus1Geo = new THREE.TorusGeometry( 2, .1, 4, 10 );
 const torus1Mat = new THREE.MeshStandardMaterial( { color:0xff00ff } );
 const torus1 = new THREE.Mesh(torus1Geo, torus1Mat);
-scene.add(torus1);
+mesh.add(torus1);
 
 const torus2Geo = new THREE.TorusGeometry( 2, .1, 4, 10 );
 const torus2Mat = new THREE.MeshStandardMaterial( { color:0x00ff00 } );
 const torus2 = new THREE.Mesh(torus2Geo, torus2Mat);
-scene.add(torus2);
+mesh.add(torus2);
 
 const torus3Geo = new THREE.TorusGeometry( 2, .1, 4, 10 );
 const torus3Mat = new THREE.MeshStandardMaterial( { color:0x0000ff } );
 const torus3 = new THREE.Mesh(torus3Geo, torus3Mat);
-scene.add(torus3);
+mesh.add(torus3);
 
 const torus4Geo = new THREE.TorusGeometry( 2, .1, 4, 10 );
 const torus4Mat = new THREE.MeshStandardMaterial( { color:0xff0000 } );
 const torus4 = new THREE.Mesh(torus4Geo, torus4Mat);
-scene.add(torus4);
+mesh.add(torus4);
 
 // -------------------------------------------------------- Lights
-const icoLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
-mesh.add(icoLight);
+//const icoLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
+//mesh.add(icoLight);
+
+const pointLight01 = new THREE.DirectionalLight(0xffffff, 1);
+pointLight01.position.set(10, 10, 10);
+scene.add(pointLight01);
+
+//const pointLight01Helper = new THREE.DirectionalLightHelper(pointLight01);
+//scene.add(pointLight01Helper);
 
 // -------------------------------------------------------- Animation Loop
 function animate() {
