971e645f11a1e48adb23710c6cd87b1d49f2b967
daummm <50vjtpsxm@naver.com>
2025-02-21 21:05:09 -0600

    .

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
 import * as THREE from 'three';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 500 );
 
 const renderer = new THREE.WebGLRenderer();
+// const loader = new GLTFLoader();
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+camera.position.set(0,0,5);
+camera.lookAt(0,0,0);
+
+
+// points.push(new THREE.Vector3(-10, 0, 0));
+// points.push(new THREE.Vector3(0,10,0));
+// points.push(new THREE.Vector3(10,0,0));
+// points.push(new THREE.Vector3(-10,0,0));
+
+// const geometry = new THREE.BufferGeometry().setFromPoints(points);
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+// const geometry = new THREE.CircleGeometry(1,100,Math.PI, Math.PI);
+const geometry = new THREE.ConeGeometry(1,1,32,1,false,0,2*Math.PI);
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material);
+scene.add(cube);
+
+// const line = new THREE.Line(geometry, material);
+// scene.add(line);
+// renderer.render(scene , camera);
 
 function animate() {
-    renderer.render( scene, camera );
+    requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
 }
-renderer.setAnimationLoop( animate );
+
+animate();
+// renderer.setAnimationLoop( animate );
