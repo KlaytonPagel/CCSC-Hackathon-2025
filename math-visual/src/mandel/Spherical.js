4199991f202f5859b3706f60f8265febded36134
Klayton Pagel <147002610+KlaytonPagel@users.noreply.github.com>
2025-02-23 22:56:58 -0600

    Reorganized the files

--- math-visual/src/mandel/Spherical.js
+++
@@ -1,9 +0,0 @@
-
-export class Spherical {
-
-    constructor(x, y, z) {
-        this.r = Math.sqrt(x*x + y*y + z*z);
-        this.theta = Math.atan2(Math.sqrt(x*x+y*y), z);
-        this.phi = Math.atan2(y, x);
-    }
-}
